ab4e3e7d1842d8f6930e232ec27316157496c78e
dute0987 <dute0987@gmail.com>
2022-02-22 11:48:15 +0530

    Program in line Comparison for Equal Great and Less

---
+++ UC-3-LineGretOrLessOrEqual.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3_lineComparison
+{
+    public class LineGretOrLessOrEqual
+    {
+        public static void CheckLineEqualLessGreater()
+        {
+            double x1, x2, x3, x4, y1, y2, y3, y4;
+            double LengthOfLine1, LengthOfLine2;
+
+            Console.WriteLine("Enter the value for 1st line");
+
+            Console.WriteLine("Please Enter The Value for x1 : ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for x2 : ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for y1 : ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for y2 : ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value for 2nd line");
+
+            Console.WriteLine("Please Enter The Value for x3 : ");
+            x3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for x4 : ");
+            x4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for y3 : ");
+            y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for y4 : ");
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            LengthOfLine1 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("the length of Line1 is :- " + LengthOfLine1);
+            LengthOfLine2 = Math.Sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+            Console.WriteLine("the length of Line2 is :- " + LengthOfLine2);
+
+
+            Console.WriteLine("comparison of 2 lines :- ");
+            Console.WriteLine("--------------------------");
+            
+            if (LengthOfLine1.Equals(LengthOfLine2))
+            {
+                Console.WriteLine("they are equal");
+            }
+            else if (LengthOfLine1 > LengthOfLine2 )
+            {
+                Console.WriteLine("Length1 is Greater than Length2");
+            }
+            else
+            {
+                Console.WriteLine("Length1 is less than Length2");
+            }
+        }
+    }
+}
