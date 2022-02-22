0952a81350d05d5633f7e500b1c84af90fe5e429
dute0987 <dute0987@gmail.com>
2022-02-22 11:28:45 +0530

    Greeting Message for LineComparison problem

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Day_3_lineComparison
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to the Program of Comparison of Line");
+            //LengthofLineCal.LengthCalculation();
+            //CheckEqualityOfTwoLine.TwoLineEquality();
+            //LineGretOrLessOrEqual.CheckLineEqualLessGreater();
+        }
+    }
+}
+
